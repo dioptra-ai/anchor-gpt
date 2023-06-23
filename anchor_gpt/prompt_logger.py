4702cfbe496f0f49271ea1e021a642dc4a776ca6
Pierre <pierre@dioptra.ai>
2023-06-23 17:18:39 -0400

    Response to prompts, store purge, rename and test

--- anchor_gpt/prompt_logger.py
+++ anchor_gpt/prompt_logger.py
@@ -1,38 +1,55 @@
-from .prompt_store import SQLitePromptStore
+import pandas as pd
+from .prompt_store import prompt_to_db, db_to_prompt, SQLitePromptStore
 from .coreset import coreset
 
 class PromptLogger:
-    def __init__(self, retriever, store=None):
-        '''
-        retriever: a function that takes a PromptStore and a number and returns a list of prompts. See the example.py file for an example.
-        store: a PromptStore object. If None, a SQLitePromptStore will be used.
-        '''
-        self.retriever = retriever
+    def __init__(self, store=None):
+        """
+        A prompt logger that stores prompts in a PromptStore.
+
+        Parameters:
+            store: a PromptStore object. If None, a SQLitePromptStore will be used.
+        """
         self.store = store or SQLitePromptStore()
-    
+
     def log(self, prompt):
-        '''
-        prompt: a Prompt object.
-        '''
+        """
+        Log a prompt to the store.
+
+        Parameters:
+            prompt: a Prompt object.
+        """
         prompt._set_store(self.store)
 
         return self.store.add(prompt)
 
-    def retrieve_n(self, n):
-        '''
-        Retrieve the top n prompts according to the retriever function passed to the constructor.
-        n: the number of prompts to retrieve.
-        '''
-        return self.retriever(self.store, n)
+    def retrieve(self, retriever, threshold):
+        """
+        Retrieve the top n prompts according to the retriever function.
 
-    def get_prompts_coreset(self, prompts, size, distance='euclidean'):
-        '''
-        Coreset is a technique that filters a dataset down to a subset while preserving diversity.
-        prompts: a list of Prompt objects.
-        size: the number of prompts to select.
-        distance: the distance function to use for the coreset algorithm (default: euclidean).
-        '''
-        prompt_ids = [p.id for p in prompts]
+        Parameters:
+            retriever: a function that takes a PromptStore and threshold a number and returns a list of prompts.
+            threshold: a theshold to be consumed by the retreiver.
+        """
+        return retriever(self.store, threshold)
+
+    def deduplicate(self, prompts, size, distance='euclidean'):
+        """
+        Remove text duplicates and embeddings near duplicates using the coreset algorithm.
+        More details on the coreset algorithm can be found here: https://arxiv.org/abs/1708.00489
+
+        Parameters:
+            prompts: a list of Prompt objects.
+            size: the number of prompts to select.
+            distance: the distance function to use for the coreset algorithm (default: euclidean).
+        """
+
+        data = [{'id': prompt.id, 'text': prompt.text} for prompt in prompts]
+
+        df = pd.DataFrame(data)
+        df = df.drop_duplicates(subset=['text'])
+
+        prompt_ids = list(df['id'])
         selected_indexes = coreset(
             vector_ids=prompt_ids,
             already_selected=[],
